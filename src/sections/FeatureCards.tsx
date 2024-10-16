170886813263a47f6814dfe0fdaacc305f21a5f0
Augusto Alexandre01 <augustoams01@gmail.com>
2024-10-16 10:42:48 -0300

    home-page-featureCards-conclusion

--- src/sections/FeatureCards.tsx
+++ src/sections/FeatureCards.tsx
@@ -1,41 +1,88 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { twMerge } from "tailwind-merge";
+
+const cardData = [
+    {
+        image: '/assets/images/pill.png',
+        title: 'Revolutionary BlockChain API',
+        description: 'Effortlessly integrate and menage blockchain date with our cuttin-edge API, designed for seamless connectivity',
+        color: 'fuchsia'
+    },
+    {
+        image: '/assets/images/cuboid.png',
+        title: 'Decentralized Data Solutions',
+        description: 'Empower your applications with decentralized data solutions, ensuring security and transparency at every step',
+        color: 'lime'
+    },
+    {
+        image: '/assets/images/cone.png',
+        title: 'Next-Gen Smart Contracts',
+        description: 'Unlock the potential of next-gen smart contracts with our robust and scalable API, tailored for modern blockchain needs.',
+        color: 'cyan'
+    },
+    {
+        image: '/assets/images/icosahedron.png',
+        title: 'Seamless Blockchain Integration',
+        description: 'Integrate blockchain techgnology seamlessly into your projects, with minimal effort and maximum efficiency',
+        color: 'violet'
+    },
+]
 
 export const FeatureCards = () => {
     return (
-        <section className="py-24">
+        <section className="py-24 overflow-x-clip md:-mt-28">
             <div className="container"> 
-                <h2 className="text-4xl font-heading font-black text-center">Discover the future of blockchain with BlockForge</h2>
-                <div className="mt-36">
-                    <div className="relative z-0 p-8 max-w-xs group">
-                        <div className="absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300"></div>
-                        <div className="absolute size-16 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsia-300 transition duration-300 top-1.5 right-1.5 -z-10"></div>
-                        <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
-                        <div className="p-4 ">
-                            <div className="flex justify-center -mt-20 relative">
-                                <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]">
-                                </div>
-                                <img src="/assets/images/pill.png" alt="Pill Image" className="size-40 group-hover:-translate-y-6 transition duration-300"/>
-                            </div>
-                            <h3 className="text-3xl font-black font-heading mt-12">Revolutionary BlockChain API</h3>
-                            <p className="text-lg text-zinc-400 mt-4">Effortlessly integrate and menage blockchain date with our cuttin-edge API, designed for seamless connectivity</p>
-                            <div className="flex items-center justify-between mt-12">
-                                <button className="text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500">Learn More</button>
-                                <FaLongArrowAltRight className="size-5 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"/>
-                            </div>
-                        </div>
-                        <div className="flex justify-center">
-                            <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full">
+                <h2 className="text-4xl md:text-5xl font-heading font-black lg:text-6xl text-center">Discover the future of blockchain with BlockForge</h2>
+                <div className="mt-36 lg:mt-48 flex">
+                    <div className="flex flex-none gap-8">
 
-                                {[...new Array(4)].fill(0).map((_, i)=>(
-                                    <div key={i} className="size-2.5 bg-zinc-500 rounded-full cursor-pointer">
-                                        
+                        {cardData.map(({image, title, description, color}) => (
+                            <div className="relative z-0 p-8 md:p-10 max-w-xs md:max-w-md group" key={title}>
+                            <div className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300", 
+                                color === "lime" && "bg-lime-500",
+                                color === "cyan" && "bg-cyan-500",
+                                color === "violet" && "bg-violet-500",
+                            )}></div>
+                            <div className={twMerge("absolute size-16 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsia-300 transition duration-300 top-1.5 right-1.5 -z-10", 
+                                color === "lime" && "bg-lime-500 group-hover:bg-lime-400",
+                                color === "cyan" && "bg-cyan-500 group-hover:bg-cyan-400",
+                                color === "violet" && "bg-violet-500 group-hover:bg-violet-400",
+                            )}></div>
+                            <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
+                            <div className="p-4 ">
+                                <div className="flex justify-center -mt-20 relative">
+                                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]">
                                     </div>
-                                ))}
-
+                                    <img src={image} alt="Pill Image" className="size-40 group-hover:-translate-y-6 transition duration-300"/>
+                                </div>
+                                <h3 className="text-3xl font-black font-heading mt-12">{title}</h3>
+                                <p className="text-lg text-zinc-400 mt-4">{description}</p>
+                                <div className="flex items-center justify-between mt-12">
+                                    <button className={twMerge("text-sm font-heading uppercase font-extrabold tracking-wider text-fuchsia-500", 
+                                        color === "lime" && "text-lime-500",
+                                        color === "cyan" && "text-cyan-500",
+                                        color === "violet" && "text-violet-500"
+                                    )}>Learn More</button>
+                                    <FaLongArrowAltRight className="size-5 text-zinc-500 group-hover:text-zinc-300 transition duration-300 -translate-x-2 group-hover:translate-x-0"/>
+                                </div>
                             </div>
+                            
                         </div>
+                        ))}
+
                     </div>
                 </div>
+
+                <div className="flex justify-center">
+                    <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full mt-10">
+
+                                {cardData.map(({title , color})=>(
+                                    <div key={title} className="size-2.5 bg-zinc-500 rounded-full cursor-pointer"></div>
+                                ))}
+
+                    </div>
+                </div>
+
             </div>
         </section>
     )
